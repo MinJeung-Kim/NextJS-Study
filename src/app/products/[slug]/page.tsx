7ca1a141c5a321f90ef1444f45e4ab386599ec95
MinJeung-Kim <focso5@gmail.com>
2023-06-04 17:32:12 +0900

    #16 SSG 렌더링

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,3 +1,6 @@
+import NotFoundPage from "@/app/not-found";
+import { getProduct, getProducts } from "@/service/products";
+
 type Props = {
   params: {
     slug: string;
@@ -10,13 +13,21 @@ export function generateMetadata({ params }: Props) {
   };
 }
 
-export default function PantsPage({ params }: Props) {
-  return <h1>{params.slug} 제품 설명 페이지</h1>;
+export default async function ProductPage({ params: { slug } }: Props) {
+  const product = await getProduct(slug);
+
+  if (!product) {
+    NotFoundPage();
+  }
+
+  // 서버 파일에 있는 데이터중 해당 제품의 정보를 찾아서 그걸 보여줌
+  return <h1>{product.name} 제품 설명 페이지</h1>;
 }
 
-export function generateStaticParams() {
-  const products = ["pants", "skirt"];
+export async function generateStaticParams() {
+  // 모든 제품의 페이지들을 미리 만들어 둘 수 있게 해줄거임 (SSG)
+  const products = await getProducts();
   return products.map((product) => ({
-    slug: product,
+    slug: product.id,
   }));
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,15 +1,18 @@
-import Link from "next/link";
+import { getProducts } from "@/service/products";
+import Link from "next/link"; 
 
-const products = ["shirt", "pants", "skirt", "shoes"];
+export default async function ProductsPage() {
 
-export default function ProductsPage() {
+  // 서버 파일(데이터베이스)에 있는 제품의 리스트를 읽어와서 , 출력
+  const products = await getProducts();
+
   return (
     <>
       <h1>제품 소개 페이지!</h1>
       <ul>
         {products.map((product, index) => (
           <li key={index}>
-            <Link href={`/products/${product}`}>{product}</Link>
+            <Link href={`/products/${product.id}`}>{product.name}</Link>
           </li>
         ))}
       </ul>
