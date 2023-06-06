b207288a6a8e1bee3d75c57d60f6e266aaf31ef0
MinJeung-Kim <focso5@gmail.com>
2023-06-06 17:16:51 +0900

    #20 API 라우트

--- src/service/products.ts
+++ src/service/products.ts
@@ -9,14 +9,9 @@ export type Product = {
 
 // 제품을 반환해주는 비동기 함수
 export async function getProducts(): Promise<Product[]> {
-    // 1. 경로 생성 (Node에서 제공해주는 path 사용)
-    // 2. path.join(현재경로, json이 있는 폴더, 해당 파일명);
-  const filePath = path.join(process.cwd(), "data", "products.json");
-  // 3. fs : Node에서 제공해주는 file 입출력이 가능한 모듈.
-  // 4. fs.readFile(생성한 경로, 인코딩 방식);
-  const data = await fs.readFile(filePath, "utf-8");
-  // 5. Object로 리턴.
-  return JSON.parse(data);
+    const filePath = path.join(process.cwd(), "data", "products.json");
+   const data = await fs.readFile(filePath, "utf-8");
+   return JSON.parse(data);
 }
 
 // 사용자가 특정 경로로 갔을때 slug(id)를 전달해주면 제품에대한 객체 리턴.
