172f5d13a9ba71bdb865def6501e4b13d8277849
MinJeung-Kim <focso5@gmail.com>
2023-06-06 22:50:00 +0900

    #21 Image Tag

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,14 +1,17 @@
 import MeowArticle from "@/components/MeowArticle";
 import { getProducts } from "@/service/products";
-import Link from "next/link"; 
+import Image from "next/image";
+import Link from "next/link";
+import clothesImage from "public/images/clothes.jpg";
 
 export default async function ProductsPage() {
-  throw new Error(); // error page test
   const products = await getProducts();
 
   return (
     <>
       <h1>제품 소개 페이지!</h1>
+      {/* 경로보다는 로컬에 있으므로 Static하게 import하는게 좋음. */}
+      <Image src={clothesImage} alt="Clothes" priority />
       <ul>
         {products.map((product, index) => (
           <li key={index}>
