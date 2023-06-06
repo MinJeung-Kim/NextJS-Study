b97d5772b6849208f9b3cea5ec70685c62cc62e2
MinJeung-Kim <focso5@gmail.com>
2023-06-06 23:30:16 +0900

    #22 Redirect

--- next.config.js
+++ next.config.js
@@ -3,6 +3,24 @@ const nextConfig = {
   images: {
     remotePatterns: [{ protocol: "https", hostname: "images.unsplash.com" }],
   },
+  async redirects() {
+    return [
+      {
+        // 영원히 삭제된 페이지 접근시.
+        source: "/products/deleted_forever",
+        // products페이지로 이동.
+        destination: "/products",
+        // status : 308
+        permanent: true,
+      },
+      {
+        // 임시 삭제된 페이지 접근시.
+        source: "/products/deleted_temp",
+        destination: "/products",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
